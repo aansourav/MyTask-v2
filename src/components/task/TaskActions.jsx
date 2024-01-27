2af1491d03a519db0ced21158378ab73a3a7c630
SoUrAv <aansourav@gmail.com>
2024-01-27 16:20:39 +0600

    Delete All button validation added

--- src/components/task/TaskActions.jsx
+++ src/components/task/TaskActions.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
+import { TaskContext } from "../../context";
 
 const TaskActions = ({ onAddTask: handleAddTask, handleDeleteAll }) => {
+  const { state } = useContext(TaskContext);
+
   return (
     <div className="mb-14 items-center justify-between sm:flex">
       <h2 className="text-2xl font-semibold max-sm:mb-4">Your Tasks</h2>
@@ -13,8 +16,10 @@ const TaskActions = ({ onAddTask: handleAddTask, handleDeleteAll }) => {
         </button>
         <button
           onClick={handleDeleteAll}
-          
-          className="rounded-md bg-red-500 px-3.5 py-2.5 text-sm font-semibold"
+          className={`rounded-md ${
+            state.tasks.length <= 0 ? "bg-gray-400" : "bg-red-500"
+          } px-3.5 py-2.5 text-sm font-semibold`}
+          disabled={state.tasks.length <= 0}
         >
           Delete All
         </button>
